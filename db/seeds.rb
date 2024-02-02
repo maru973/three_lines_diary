b483c4777359b7a33ea4f527ae025f266227872a
honoka <ara4wa12.market@gmail.com>
2024-02-02 13:24:47 +0900

    Faker追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  User.create!(
+    last_name: Faker::Name.last_name,
+    first_name: Faker::Name.first_name,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+user_ids = User.ids
+
+30.times do |index|
+  user = User.find(user_ids.sample)
+  user.diaries.create!(first_line: "first_line#{index}", second_line: "second_line#{index}", third_line: "third_line#{index}")
+end
