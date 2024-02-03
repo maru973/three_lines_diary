6a1085b458bdf25baaf39367c45f5966ecb128be
gudemaru <gudemaru@gudemarunoMacBook-Air.local>
2024-02-03 23:28:35 +0900

    プロフィール画像の追加

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_03_124318) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatars"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
