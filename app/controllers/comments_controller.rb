f77b00ba9d6389b719d6208ed9f06cf9152fc162
gudemaru <gudemaru@gudemarunoMacBook-Air.local>
2024-02-03 17:27:03 +0900

    turbo_streamを使ったコメント編集機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.build(comment_params)
+    @diary = Diary.find(params[:diary_id])
+    # 現在のユーザーと取得したdiaryに紐づく新しいcommentをビルドする
+    @comment = current_user.comments.build(comment_params.merge(diary: @diary))
     @comment.save
-  end
-
-  def show
-    @comment = current_user.comments.find(params[:id])
   end
 
   def edit
@@ -25,6 +23,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(diary_id: params[:diary_id])
+    params.require(:comment).permit(:body)
   end
 end
