2a3db8022fc072569feb2f3e790632a2b73bf77f
honoka <ara4wa12.market@gmail.com>
2024-04-18 15:50:27 +0900

    テスt

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,4 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   delete 'logout' => 'user_sessions#destroy', :as => :logout
-
-  #テスト
 end
